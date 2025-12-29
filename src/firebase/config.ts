98c7c4d55ac61e355fd4096eb69807e6999de317
TIRTHANKAR DASGUPTA <tirthankar.dgen@gmail.com>
2025-12-29 16:51:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5450835271-19abf",
+  "appId": "1:1055374469856:web:c41a5898148a1369cebe85",
+  "apiKey": "AIzaSyBcyrMvlRr8FeZD7gkauSGSPu3gzrg8zxw",
+  "authDomain": "studio-5450835271-19abf.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1055374469856"
+};
